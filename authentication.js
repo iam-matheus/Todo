7c978b2af585b7d9f6adacef4467db3da4dd3e6e
Mathews01 <mathewsmulundu01@gmail.com>
2021-11-05 06:54:04 +0200

    Tired defining firebase in authentication, probably didn't work

--- authentication.js
+++ authentication.js
@@ -5,56 +5,65 @@
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   
   
-  import { initializeApp } from 'firebase/app';
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyBkMC1VSn6dNCTcYk6jsnDyYv4MVCY4AX8",
-    authDomain: "to-do-app-e691f.firebaseapp.com",
-    projectId: "to-do-app-e691f",
-    storageBucket: "to-do-app-e691f.appspot.com",
-    messagingSenderId: "1007999147036",
-    appId: "1:1007999147036:web:29c87a991f6f63606d53d9",
-    measurementId: "G-8MHCPYYW7F"
-  };
+import { initializeApp } from 'firebase/app';
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+$.getScript('https://www.gstatic.com/firebasejs/3.2.1/firebase.js', function () {
 
-  firebase.auth.Auth.Persistance.LOCAL;
+    /**
+     * initialize
+     * @type {{apiKey: string, authDomain: string, databaseURL: string, storageBucket: string}}
+     */
 
-  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-  const auth = getAuth();
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
-  })
-  .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-  });
+    const firebaseConfig = {
+        apiKey: "AIzaSyBkMC1VSn6dNCTcYk6jsnDyYv4MVCY4AX8",
+        authDomain: "to-do-app-e691f.firebaseapp.com",
+        projectId: "to-do-app-e691f",
+        storageBucket: "to-do-app-e691f.appspot.com",
+        messagingSenderId: "1007999147036",
+        appId: "1:1007999147036:web:29c87a991f6f63606d53d9",
+        measurementId: "G-8MHCPYYW7F"
+    };
 
-  $("#submit-btn").click(function(){
-      let email = $("#email").val();
-      let password = $("#password").val();
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
 
-      if(email != "" && password != ""){
-           let results = firebase.auth().signInWithEmailAndPassword(email, password);
+    firebase.auth.Auth.Persistance.LOCAL;
 
-           results.catch(function(error){
-            let errorCode = error.code;
-            let errorMessage = error.message;
+    import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-            console.log(errorCode);
-            console.log(errorMessage);
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
 
-            alert("Message: " + errorMessage);
-           });
-      }
-      else{
-          alert("Hey, fill out all the fields.")
-      }
-  })
+    $("#submit-btn").click(function(){
+        let email = $("#email").val();
+        let password = $("#password").val();
+
+        if(email != "" && password != ""){
+            let results = firebase.auth().signInWithEmailAndPassword(email, password);
+
+            results.catch(function(error){
+                let errorCode = error.code;
+                let errorMessage = error.message;
+
+                console.log(errorCode);
+                console.log(errorMessage);
+
+                alert("Message: " + errorMessage);
+            });
+        }
+        else{
+            alert("Hey, fill out all the fields.")
+        }
+    })
+})
